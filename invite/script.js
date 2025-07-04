3e9b9718f488abd996b398e6769d21622d385f6e
tomcat with a q (git.tomcat.sh) <hi@tomcat.sh>
2025-07-04 18:10:21 -0400

    Create script.js

---
+++ invite/script.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+  window.location.href = "https://discord.com/oauth2/authorize?client_id=1390815472412397701&integration_type=1&scope=applications.commands";
+}; // auto redirect :P
